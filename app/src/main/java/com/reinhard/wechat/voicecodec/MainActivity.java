3499c4af91024151b0613158bae96c4c14975c73
Q <vfishv@gmail.com>
2023-12-22 05:57:32 +0800

    to androidx

--- app/src/main/java/com/reinhard/wechat/voicecodec/MainActivity.java
+++ app/src/main/java/com/reinhard/wechat/voicecodec/MainActivity.java
@@ -4,7 +4,7 @@ import android.Manifest;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.Environment;
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
